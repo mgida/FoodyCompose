06b04597230db7803f0851f5fa5ee0bc952978e0
mgida <mahmoudgida82@gmail.com>
2024-07-17 23:01:10 +0300

    - call SaveRecipeUseCase, DeleteRecipeUseCase in favourites screen

--- app/src/main/java/com/example/foody/feature_recipe/presentation/fav_screen/FavouriteRecipesEvent.kt
+++ app/src/main/java/com/example/foody/feature_recipe/presentation/fav_screen/FavouriteRecipesEvent.kt
@@ -1,5 +1,9 @@
 package com.example.foody.feature_recipe.presentation.fav_screen
 
+import com.example.foody.feature_recipe.domain.model.RecipeModel
+
 sealed class FavouriteRecipesEvent {
     data object GetRecipes : FavouriteRecipesEvent()
+    data class DeleteRecipe(val recipeModel: RecipeModel) : FavouriteRecipesEvent()
+    data class SaveRecipe(val recipeModel: RecipeModel) : FavouriteRecipesEvent()
 }
--- app/src/main/java/com/example/foody/feature_recipe/presentation/fav_screen/components/FavouriteRecipesScreen.kt
+++ app/src/main/java/com/example/foody/feature_recipe/presentation/fav_screen/components/FavouriteRecipesScreen.kt
@@ -6,10 +6,15 @@ import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -21,6 +26,8 @@ import com.example.foody.feature_recipe.presentation.fav_screen.FavouriteRecipes
 import com.example.foody.feature_recipe.presentation.fav_screen.viewmodel.FavouriteRecipesViewModel
 import com.example.foody.feature_recipe.presentation.search_recipes_screen.components.RecipesSearchResult
 import com.example.foody.feature_recipe.presentation.search_recipes_screen.components.SearchRecipesShimmerItems
+import kotlinx.coroutines.flow.collectLatest
+import timber.log.Timber
 
 
 @OptIn(ExperimentalSharedTransitionApi::class)
@@ -32,47 +39,92 @@ fun FavouriteRecipesScreen(
     animatedVisibilityScope: AnimatedContentScope,
 ) {
 
+    val snackbarHostState = remember { SnackbarHostState() }
+    val recipeSavedMessage = stringResource(id = R.string.recipe_saved_to_favourites)
+    val recipeDeletedMessage = stringResource(id = R.string.recipe_removed_from_favourites)
+    val actionLabel = stringResource(R.string.undo)
+
     LaunchedEffect(key1 = true) {
         viewModel.onEvent(FavouriteRecipesEvent.GetRecipes)
     }
 
-    val favState = viewModel.favState.collectAsState().value
-
-
-    Surface(modifier = modifier) {
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(vertical = 16.dp, horizontal = 8.dp)) {
-            when {
-                favState.isLoading -> {
-                    SearchRecipesShimmerItems()
-                }
-
-                favState.error.isNotBlank() -> {
-                    ErrorState(
-                        modifier = Modifier.fillMaxSize(),
-                        errorMsg = favState.error
+    LaunchedEffect(key1 = viewModel.eventFlow) {
+        viewModel.eventFlow.collectLatest { value: FavouriteRecipesViewModel.UiEvent ->
+            when (value) {
+                is FavouriteRecipesViewModel.UiEvent.DeleteRecipe -> {
+                    Timber.i("Foody.. recipe deleted")
+                    val snackbarResult = snackbarHostState.showSnackbar(
+                        message = recipeDeletedMessage,
+                        actionLabel = actionLabel
                     )
+
+                    when (snackbarResult) {
+                        SnackbarResult.ActionPerformed -> {
+                            viewModel.onEvent(FavouriteRecipesEvent.SaveRecipe(value.recipeModel))
+                        }
+
+                        SnackbarResult.Dismissed -> Unit
+                    }
                 }
 
-                favState.recipes.isEmpty() -> {
-                    EmptyResult(
-                        modifier = Modifier.fillMaxSize(),
-                        msg = stringResource(R.string.nothing_found)
-                    )
-                }
-
-                else -> {
-                    RecipesSearchResult(
-                        favState.recipes,
-                        sharedTransitionScope = sharedTransitionScope,
-                        animatedVisibilityScope = animatedVisibilityScope,
-                        onRecipeClicked = {},
-                        onFavClicked = {}
+                is FavouriteRecipesViewModel.UiEvent.SaveRecipe -> {
+                    Timber.i("Foody.. recipe saved")
+                    snackbarHostState.showSnackbar(
+                        message = recipeSavedMessage
                     )
                 }
             }
         }
     }
+
+
+    val favState = viewModel.favState.collectAsState().value
+
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        content = { paddingValues ->
+            Surface(modifier = modifier.padding(paddingValues)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(vertical = 16.dp, horizontal = 8.dp)
+                ) {
+                    when {
+                        favState.isLoading -> {
+                            SearchRecipesShimmerItems()
+                        }
+
+                        favState.error.isNotBlank() -> {
+                            ErrorState(
+                                modifier = Modifier.fillMaxSize(),
+                                errorMsg = favState.error
+                            )
+                        }
+
+                        favState.recipes.isEmpty() -> {
+                            EmptyResult(
+                                modifier = Modifier.fillMaxSize(),
+                                msg = stringResource(R.string.nothing_found)
+                            )
+                        }
+
+                        else -> {
+                            RecipesSearchResult(
+                                favState.recipes,
+                                sharedTransitionScope = sharedTransitionScope,
+                                animatedVisibilityScope = animatedVisibilityScope,
+                                onRecipeClicked = {},
+                                onFavClicked = { recipeModel ->
+                                    viewModel.onEvent(FavouriteRecipesEvent.DeleteRecipe(recipeModel = recipeModel))
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    )
+
 
 }
